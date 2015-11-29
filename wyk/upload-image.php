2cc7586b87af55c4c78143cf6e1e095b8f5970ca
Tomasz Kardas <kardi31@tlen.pl>
2015-11-29 18:02:31 +0100

    ogloszaenia

--- wyk/upload-image.php
+++ wyk/upload-image.php
@@ -19,12 +19,34 @@ if($_SESSION['user_id']>=1)
 	$data = base64_decode($img);
         $filename =  uniqid() . '.png';
 	$file = UPLOAD_DIR .$filename;
-	$success = file_put_contents($file, $data);
         
-        $up="UPDATE ".$pre."mov SET fo_thumb='".$filename."' WHERE fo_id='".$id."'";
-        db_query($up);
         
-        echo json_encode(array('url' => $file));
+            $success = file_put_contents($file, $data);
+
+        if(!check_if_black($file)){
+            $up="UPDATE ".$pre."mov SET fo_thumb='".$filename."' WHERE fo_id='".$id."'";
+            db_query($up);
+
+            echo json_encode(array('url' => $file));
+        }
+        else{
+            unlink($file);
+        }
         
 }
+
+function check_if_black($src){
+
+            $img = imagecreatefrompng($src);
+            list($width_orig, $height_orig)=getimagesize($src);
+            for($i=0;$i<20;$i++){
+                $rand_width=rand ( 0 , $width_orig );
+                $rand_height=rand ( 0 , $height_orig );
+                $rgb = imagecolorat($img, $rand_width, $rand_height);
+                if($rgb!=0){
+                    return false;
+                }
+            }
+            return true;
+        }
 ?>
